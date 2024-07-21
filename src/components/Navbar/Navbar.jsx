2cc5f7492553c78ec4b36dbe3a2ce5c5af5019cc
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-07-21 19:03:16 +0100

    Refactor Navbar to use card component.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
+import Card from "../Card/Card";
 import "./Navbar.css"
 
 const Navbar = () => {
     return ( 
-        <div className="navbar">
+        <Card id="navbar">
             <a className="navlinks" href="#me">Me</a>
             <a className="navlinks" href="#projects">Projects</a>
             <a className="navlinks" href="#resume">Resume</a>
             <a className="navlinks" href="#contact">Contact</a>
-        </div>
+        </Card>
      );
 }
  
