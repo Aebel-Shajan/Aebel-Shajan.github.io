4a26a618f37032ab482ec5bfbc457ab58b66930c
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-07-22 21:53:05 +0100

    Add Projects section. Add function which fetches repos from api.github.com.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import About from './components/Sections/About/About'
+import Projects from './components/Sections/Projects/Projects'
 
 function App() {
   return (
-    <div className='app'>
+    <div className="app">
       <Navbar />
-      <About />
+      <div id="inner-body">
+        <About />
+        <Projects />
+      </div>
     </div>
   )
 }
---
+++ src/components/Sections/Projects/Projects.jsx
@@ -0,0 +1,62 @@
+import Card from "../../Card/Card";
+import "./Projects.css";
+import projectLinks from "./../../../assets/data/projects.json"
+import { useEffect, useState } from "react";
+
+
+
+const Projects = () => {
+    const [projectRepos, setProjectRepos] = useState([])
+
+    const getProjectRepos = async (githubLinks) => {
+        const response = await fetch("https://api.github.com/users/aebel-shajan/repos?per_page=100");
+        if (!response.ok) {
+            throw new Error(`Failed to fetch repos! status: ${response.status}`)
+        }
+        let githubRepos = await response.json();
+        githubRepos = githubRepos.filter((repo) => {
+            return githubLinks.includes(repo["html_url"])
+        })
+        setProjectRepos(githubRepos)
+        return githubRepos
+    }
+
+    useEffect(() => {
+        getProjectRepos(projectLinks)
+    }, [])
+    
+    return (
+        <section id="projects">
+            <Card id="projects-title">
+                <h1>Projects</h1>
+            </Card>
+            <Card id="projects-form">
+                <form action="/search" method="GET">
+                    <input 
+                        type="text" 
+                        name="query" 
+                        placeholder="Search..." 
+                        className="search-input" />
+                    <button 
+                        type="submit" 
+                        className="search-button">
+                        Search
+                    </button>
+                </form>
+            </Card>
+            <div id="project-cards">
+                {
+                    projectRepos.map(project => {
+                        return (
+                            <Card>
+                                {project.name}
+                            </Card>
+                        )
+                    })
+                }
+            </div>
+        </section >
+     );
+}
+
+export default Projects;
