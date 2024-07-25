9f21622a669c0927037ef64a7b3d89f6f52ce95b
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-07-25 14:47:10 +0100

    Change design of project card. Make project image div clickable.

--- src/components/Sections/Projects/ProjectCard/ProjectCard.jsx
+++ src/components/Sections/Projects/ProjectCard/ProjectCard.jsx
@@ -1,16 +1,27 @@
 import Card from "@/components/Card/Card";
 import "./ProjectCard.css";
+import { FaGithub} from "react-icons/fa";
 
 const ProjectCard = ({ projectData}) => {
     const projectDate = new Date(projectData["created_at"])
+    const projectLink = projectData["homepage"] ? projectData["homepage"] : projectData["html_url"]
+    const openInNewTab = (url) => {
+        window.open(url, "_blank", "noreferrer");
+      };
+    
     return (
-        <Card className="project-card">
+        <Card 
+            className="project-card"
+            >
             <a 
                 className="github-link-overlay"
                 href={projectData["html_url"]} >
-                Source
+                <FaGithub />
             </a>
-            <div className="project-image">
+            <div 
+                className="project-image"
+                onClick={() => openInNewTab(projectLink)}
+                >
             </div>
             <div className="project-title">
                 {projectData["name"]}
